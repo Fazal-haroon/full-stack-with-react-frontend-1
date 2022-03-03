3b0aba673c82cb1b998e85c82dfd3806229b2cbe
Fazal Haroon <kharoon187@gmail.com>
2022-03-04 03:47:13 +0500

    Handling click event on the increment button and adding state to a react counter component

--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -1,12 +1,30 @@
-import './Counter.css'
+import React, { Component } from "react";
+import "./Counter.css";
+export default class counter extends Component {
+  //Define the initial state in a constructor
+  //state => counter 0
+  constructor() {
+    super(); //this is error 1
+    this.state = {
+      counter: 0,
+    };
+    this.increment = this.increment.bind(this);
+  }
 
-function Counter() {
-    return ( 
-        <div className="counter">
-            <button>+1</button>
-            <span className='count'>0</span>
-        </div>
-        );
+  render() {
+    return (
+      <div className="counter">
+        <button onClick={this.increment}>+1 </button>
+        <span className="count">{this.state.counter}</span>
+      </div>
+    );
+  }
+  increment() {
+    //Update state - counter++
+    //console.log('increment')
+    //this.state.counter++; //bad practice
+    this.setState({ 
+        counter: this.state.counter + 1 
+    });
+  }
 }
-
-export default Counter
